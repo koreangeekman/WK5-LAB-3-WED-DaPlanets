711c6eea9d132b90e116e944cc7451673e76608e
DJ Aaron <koreangeekman@gmail.com>
2023-10-18 14:50:56 -0600

    added getbyid + create colony

--- server/src/controllers/ColonyController.js
+++ server/src/controllers/ColonyController.js
@@ -1,3 +1,4 @@
+import { Auth0Provider } from "@bcwdev/auth0provider";
 import { colonyService } from "../services/ColonyService.js";
 import BaseController from "../utils/BaseController.js";
 
@@ -6,6 +7,9 @@ export class ColonyController extends BaseController {
     super('api/colonies')
     this.router
       .get('', this.getColonies)
+      .get('/:colonyId', this.getColonyById)
+      .use(Auth0Provider.getAuthorizedUserInfo)
+      .post('', this.createColony)
   }
 
   async getColonies(req, res, nxt) {
@@ -16,4 +20,26 @@ export class ColonyController extends BaseController {
       nxt(error)
     }
   }
+
+  async getColonyById(req, res, nxt) {
+    try {
+      const colony = await colonyService.getColonyById(req.params.colonyId);
+      return res.send(colony)
+    } catch (error) {
+      nxt(error)
+    }
+  }
+
+  // vv AUTHORIZED USERS ONLY vv
+
+  async createColony(req, res, nxt) {
+    try {
+      req.body.creatorId = req.userInfo.id
+      const newColony = await colonyService.createColony(req.body);
+      return res.send(newColony)
+    } catch (error) {
+      nxt(error)
+    }
+  }
+
 }
--- server/src/services/ColonyService.js
+++ server/src/services/ColonyService.js
@@ -8,6 +8,17 @@ class ColonyService {
     const colonies = await dbContext.Colonies.find();
     return colonies;
   }
+  async getColonyById(colonyId) {
+    const colony = await dbContext.Colonies.findById(colonyId);
+    return colony;
+  }
+
+  // vv AUTHORIZED USERS ONLY vv
+
+  async createColony(body) {
+    const newColony = await dbContext.Colonies.create(body);
+    return newColony;
+  }
 
 }
 

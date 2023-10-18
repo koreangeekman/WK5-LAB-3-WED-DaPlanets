11b52ad0e625336b7fb3f4efa15a2a7b5936a1c9
DJ Aaron <koreangeekman@gmail.com>
2023-10-18 14:33:08 -0600

    added getbyid + create planet

--- server/src/controllers/PlanetController.js
+++ server/src/controllers/PlanetController.js
@@ -6,12 +6,36 @@ export class PlanetController extends BaseController {
     super('api/planets')
     this.router
       .get('', this.getPlanets)
+      .get('/:planetId', this.getPlanetById)
+      .use('', this.getPlanets)
+      .post('', this.createPlanet)
   }
 
   async getPlanets(req, res, nxt) {
     try {
       const planets = await planetService.getPlanets();
       return res.send(planets)
+    } catch (error) {
+      nxt(error)
+    }
+  }
+
+  async getPlanetById(req, res, nxt) {
+    try {
+      const planet = await planetService.getPlanetById(req.params.planetId);
+      return res.send(planet)
+    } catch (error) {
+      nxt(error)
+    }
+  }
+
+  // vv AUTHORIZED USERS ONLY vv
+
+  async createPlanet(req, res, nxt) {
+    try {
+      req.body.creatorId = req.userInfo.id
+      const newPlanet = await planetService.createPlanet(req.body);
+      return res.send(newPlanet)
     } catch (error) {
       nxt(error)
     }
--- server/src/services/PlanetService.js
+++ server/src/services/PlanetService.js
@@ -9,6 +9,19 @@ class PlanetService {
     return planets;
   }
 
+  async getPlanetById(planetId) {
+    const planet = await dbContext.Planets.findById(planetId);
+    if (!planet) { throw new BadRequest(`Unable to locate Planet by ID: ${planetId}`) }
+    return planet;
+  }
+
+  // vv AUTHORIZED USERS ONLY vv
+
+  async createPlanet(body) {
+    const newPlanet = await dbContext.Planets.create(body);
+    return newPlanet;
+  }
+
 }
 
 export const planetService = new PlanetService();
